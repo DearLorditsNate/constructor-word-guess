6036f2e9b1635a0b48f8ef84b652c9ea99bd6f6a
Nate Micinski <nmicinski@gmail.com>
2019-03-01 16:12:26 -0800

    add inquirer prompts

--- index.js
+++ index.js
@@ -1,18 +1,71 @@
+/*
+===============================
+Initialize Packages
+===============================
+*/
+
 var Word = require("./Word");
 
 var inquirer = require("inquirer");
 
+/*
+===============================
+Global Varialbes
+===============================
+*/
+
+// Word Array
 var words = [
     "WORDONE",
     "WORDTWO",
     "WORDTHREE"
 ];
 
+// Stores word object
+var word;
+
+/*
+===============================
+Function Declarations
+===============================
+*/
+
 // Get random word
 function getRandomWord(arr) {
     return arr[Math.floor(Math.random() * 3)];
 }
 
-var word = new Word(getRandomWord(words));
+// Generate new random word object
+function newRandomWord() {
+    word = new Word(getRandomWord(words));
+}
 
-console.log(word);
+// Prompt letter guess
+function promptLetterGuess() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Guess a letter!",
+            name: "letter"
+        }
+    ]).then(answers => {
+        word.hasLetter(answers.letter);
+        console.log(word.makeString());
+        promptLetterGuess();
+    });
+}
+
+// Initialize game
+function initialize() {
+    newRandomWord();
+    console.log(word.makeString());
+    promptLetterGuess();
+}
+
+/*
+===============================
+Function Calls
+===============================
+*/
+
+initialize();
